ff0bcdcc63577949756bf7c641a6318c35cfc631
Artur Herczeg <Artur_Herczeg@epam.com>
2016-01-13 15:06:42 +0100

    Code coverage generator script

---
+++ DependencyAnalyzer/Tests/CodeCoverage.ps1
@@ -0,0 +1,24 @@
+$openCoverPath = "..\packages\OpenCover.4.6.401-rc\tools\OpenCover.Console.exe"
+$nUnitPath = "..\packages\NUnit.Console.3.0.1\tools\nunit3-console.exe"
+$reportGeneratorPath = "..\packages\ReportGenerator.2.4.0-beta2\tools\ReportGenerator.exe"
+
+$testAssemblies = "AnalyzerTests\Bin\Debug\AnalyzerTests.dll"
+
+$nUnitArgs = "$testAssemblies"
+
+Write-Host "[Debug] Creating coverage report directory"
+
+if (!(Test-Path CoverageReport)) {
+    New-Item CoverageReport -type directory
+}
+
+Write-Host "[Debug] Executing OpenCover with nUnit"
+& $openCoverPath -target:$nUnitPath -targetargs:"$nUnitArgs" -register:user -output:CoverageReport/coverage.xml
+
+Write-Host "[Debug] Generating coverage report"
+& $reportGeneratorPath -reports:CoverageReport/coverage.xml -targetdir:CoverageReport
+
+Write-Host "[Debug] Opening report"
+Start-Process .\CoverageReport\index.htm
+
+Write-Host "[Debug] Done."
